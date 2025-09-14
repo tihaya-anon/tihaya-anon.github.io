af879ae3728fc670e494c3a488e07b90794905fc
tihaya-anon <chihaya_anon_0908@outlook.com>
2025-09-14 10:36:02 +0800

    9.14

---
+++ assets/use-view-BX_QOh10.js
@@ -0,0 +1,2 @@
+import{r as t}from"./vendor_react_index.js_commonjs-es-import-Dw0Sl0kA.js";import{V as r}from"./index-CzSy3jpw.js";function n(){const e=t.useContext(r);if(e===void 0)throw new Error("useView must be used within a ViewProviderContext");return e}export{n as u};
+//# sourceMappingURL=use-view-BX_QOh10.js.map
